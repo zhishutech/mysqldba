b40a65130a80602c0fdcf7b99932e4629aaea839
xuclachina <573242930@qq.com>
2019-04-04 20:29:45 +0800

    增加open-falcon MySQL数据采集脚本

---
+++ mysql-tools/open-falcon/README.md
@@ -0,0 +1,66 @@
+| metric name                    | 指标说明                                  | 取值      | 类型    |
+| ------------------------------ | ----------------------------------------- | --------- | ------- |
+| mysql_alive                    | mysql存活状态，1为存活，0为宕机           | 自定义    | GAUGE   |
+| reponsetime                    | 响应时间，单位：秒                        | 自定义    | GAUGE   |
+| uptime                         | 存活时间，单位：秒                        | status    | GAUGE   |
+| role                           | 实例角色，1为master，0为slave             | 自定义    | GAUGE   |
+| read_only                      | 只读                                      | variables | GAUGE   |
+| super_read_only                | 管理员只读                                | variables | GAUGE   |
+| aborted_client                 | 已连接断开数                              | status    | COUNTER |
+| aborted_connections            | 为连接断开数                              | status    | COUNTER |
+| threads_connected              | 已连接线程数                              | status    | GAUGE   |
+| threads_running                | 活跃线程数                                | status    | GAUGE   |
+| threads_created                | 历史上创建过的线程数                      | status    | COUNTER |
+| Max_used_connections           | 历史最大连接数                            | status    | GAUGE   |
+| max_user_connections           | 最大用户连接数                            | variables | GAUGE   |
+| max_connections                | 最大连接数                                | variables | GAUGE   |
+| innodb_buffer_pool_size        | innodb_buffer_pool大小                    | variables | GAUGE   |
+| qps                            | 每秒查询数，queries/s                     | 自定义    | GAUGE   |
+| tps                            | 每秒事务数，commit+rollback/s             | 自定义    | GAUGE   |
+| created_tmp_disk_tables        | 创建磁盘临时表数                          | status    | COUNTER |
+| created_tmp_files              | 创建临时文件数                            | status    | COUNTER |
+| created_tmp_tables             | 创建内存临时表数                          | status    | COUNTER |
+| select_full_join               | 全表join数                                | status    | COUNTER |
+| select_full_range              | 全表range数                               | status    | COUNTER |
+| select_scan                    | 全表扫描数                                | status    | COUNTER |
+| Slow_queries                   | 慢查询数                                  | status    | COUNTER |
+| table_locks_immediate          | 立即锁表次数                              | status    | GAUGE   |
+| table_locks_waited             | 等待表锁次数                              | status    | COUNTER |
+| innodb_row_lock_current_waits  | 当前行锁等待数                            | status    | GAUGE   |
+| innodb_row_lock_time           | 行锁等待时间                              | status    | COUNTER |
+| innodb_row_lock_time_avg       | 平均行锁等待时间                          | status    | GAUGE   |
+| innodb_row_lock_waits          | 行锁等待次数                              | status    | COUNTER |
+| bytes_received                 | 接受字节数                                | status    | COUNTER |
+| bytes_sent                     | 发送字节数                                | status    | COUNTER |
+| innodb_buffer_pool_wait_free   | 等待buffer_pool分配新page数               | status    | COUNTER |
+| innodb_log_waits               | 等待刷redo log次数                        | status    | COUNTER |
+| history_list_length            | 等待被purge的队列长度，来自innodb status  | 自定义    | GAUGE   |
+| loglsn                         | 当前lsn值                                 | 自定义    | GAUGE   |
+| logflushlsn                    | 刷新到redo的lsn值                         | 自定义    | GAUGE   |
+| checkpointlsn                  | check point lsn值                         | 自定义    | GAUGE   |
+| open_files                     | 当前打开文件数                            | status    | GAUGE   |
+| opened_files                   | 历史打开文件数                            | status    | COUNTER |
+| open_tables                    | 当前打开表数                              | status    | GAUGE   |
+| opened_tables                  | 历史打开表数                              | status    | COUNTER |
+| table_open_cache               | 表缓存数                                  | variables | GAUGE   |
+| Open_table_definitions         | 当前打开表定义数                          | status    | GAUGE   |
+| opened_table_definitions       | 历史打开表定义数                          | status    | COUNTER |
+| table_definition_cache         | 表定义数                                  | variables | GAUGE   |
+| Handler_read_key               | 读取索引次数                              | status    | COUNTER |
+| Handler_read_next              | 读取下一个值次数                          | status    | COUNTER |
+| Handler_read_rnd               | 随机读次数                                | status    | COUNTER |
+| Handler_read_rnd_next          | 全表扫描或者排序或者读下一行              | status    | COUNTER |
+| sort_merge_pass                | 归并排序次数                              | status    | COUNTER |
+| binlog_cache_disk_use          | 使用磁盘存储binlog                        | status    | COUNTER |
+| slave_io_running               | slave上io线程状态，来源show slave status  | 自定义    | GAUGE   |
+| slave_sql_running              | slave上sql线程状态,来源show slave status  | 自定义    | GAUGE   |
+| Seconds_behind_master          | slave延迟（暂时），后期改为pt-slave-delay | 自定义    | GAUGE   |
+| innodb_flush_log_at_trx_commit | innodb_flush_log_at_trx_commit            | variables | GAUGE   |
+| sync_binlog                    | sync_binlog                               | variables | GAUGE   |
+| Com_select                     | select次数                                | status    | COUNTER |
+| Com_update                     | update次数                                | status    | COUNTER |
+| Com_insert                     | insert次数                                | status    | COUNTER |
+| Com_delete                     | delete次数                                | status    | COUNTER |
+| Com_commit                     | commit次数                                | status    | COUNTER |
+| Com_rollback                   | rollback次数                              | status    | COUNTER |
+
---
+++ mysql-tools/open-falcon/mymon.py
@@ -0,0 +1,143 @@
+#! /bin/env python
+# coding:gbk
+# author:xucl
+import configparser
+import requests
+import pymysql
+import time
+import json
+
+from mymon_utils import map_value, get_var_metric, get_sta_metric, get_statusdiff_metric, get_innodb_metric, get_slave_metric, get_other_metric, generate_metric
+
+
+class DBUtil:
+    def __init__(self, user=None, passwd=None, host=None, port=None, db=None, endpoint=None, tags=None):
+        self.user = user
+        self.passwd = passwd
+        self.host = host
+        self.port = port
+        self.db = db
+        self.endpoint = endpoint
+        self.tags = tags
+        self._conn = None
+        self._cursor = None
+
+    def __enter__(self):
+        self._conn = pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd, db=self.db)
+        self._cursor = self._conn.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._conn.close()
+        self._conn = None
+
+    def get_status(self):
+        self._cursor.execute('show global status;')
+        data_list = self._cursor.fetchall()
+        data_dict = {}
+        for item in data_list:
+            data_dict[item[0]] = item[1]
+        return data_dict
+
+    def get_innodb_status(self):
+        self._cursor.execute('show engine innodb status;')
+        innodb_status = self._cursor.fetchall()
+        innodb_status_format = str(innodb_status).split('\\n')
+        return innodb_status_format
+
+    def get_slave_status(self):
+        column_list = []
+        data_dict = {}
+        self._cursor.execute('show slave status;')
+        data_list = self._cursor.fetchall()
+        if data_list:
+            data_list = list(data_list[0])
+            for index, desc in enumerate(self._cursor.description):
+                data_dict[desc[0]] = data_list[index]
+        return data_dict
+
+    def get_variables(self, variable_list):
+        self._cursor.execute('show global variables;')
+        data_list = self._cursor.fetchall()
+        data_dict = {}
+        for item in data_list:
+            data_dict[item[0]] = item[1]
+        metric_list = get_var_metric(self, variable_list, data_dict, ts)
+        return metric_list
+
+    def get_status_metric(self, status, data_dict, type):
+        metric_list = get_sta_metric(self, status, data_dict, ts, type)
+        return metric_list
+
+    def get_custom_status(self, data_dict1, data_dict2):
+        metric_list = get_statusdiff_metric(self, data_dict1, data_dict2, ts)
+        return metric_list
+
+    def get_custom_metrics(self, data_dict, innodb_status_format, slave_status_format):
+        metrc_list = []
+        start_time = int(time.time())
+        self._cursor.execute("select benchmark(10000000,'select 1+1');")
+        end_time = int(time.time())
+        rt = end_time - start_time
+        innodb_metric_list = get_innodb_metric(self, innodb_status_format, ts)
+        slave_metric_list = get_slave_metric(self, slave_status_format, ts)
+        othter_metric_list = get_other_metric(self, "reponsetime", rt, ts)
+        alive_metrics = get_other_metric(self, "mysql_alive", 1, ts)
+        metrc_list.extend(innodb_metric_list)
+        metrc_list.extend(slave_metric_list)
+        metrc_list.extend(othter_metric_list)
+        metrc_list.extend(alive_metrics)
+        return metrc_list
+    
+
+
+if __name__ == '__main__':
+
+    cf = configparser.ConfigParser()
+    cf.read('mymon.cnf')
+
+    hostname = cf.get('mysql', 'HOST')
+    port = cf.get('mysql', 'PORT')
+    username = cf.get('mysql', 'USERNAME')
+    password = cf.get('mysql', 'PASSWORD')
+    endpoint = cf.get('mysql', 'ENDPOINT')
+    tags = cf.get('mysql', 'TAGS')
+
+    ts = int(time.time())
+    payload = []
+    try:
+        with DBUtil(username, password, hostname, int(port), 'information_schema', endpoint, tags) as client:
+            data_dict1 = client.get_status()
+            innodb_status_format = client.get_innodb_status()
+            slave_status_format = client.get_slave_status()
+            time.sleep(1)
+            data_dict2 = client.get_status()
+
+            variables = ['read_only', 'super_read_only', 'innodb_buffer_pool_size', 'table_open_cache', 'table_definition_cache',
+                         'innodb_flush_log_at_trx_commit', 'sync_binlog', 'max_connections', 'max_user_connections']
+            gague_status = ['Uptime', 'Threads_connected', 'Threads_created', 'Threads_running','Innodb_row_lock_current_waits',
+                            'Innodb_row_lock_time_avg', 'Open_files', 'Open_tables','Open_table_definitions', 'Max_used_connections']
+            counter_status = ['Aborted_clients', 'Aborted_connects', 'Created_tmp_disk_tables', 'Created_tmp_files', 'Created_tmp_tables',
+                              'Select_full_join', 'Select_full_range_join', 'Select_scan', 'Slow_queries', 'Table_locks_immediate',
+                              'Table_locks_waited', 'Innodb_row_lock_time', 'Innodb_row_lock_waits', 'Bytes_received', 'Bytes_sent', 'Innodb_buffer_pool_wait_free',
+                              'Innodb_log_waits', 'Opened_files', 'Opened_tables', 'Opened_table_definitions', 'Handler_read_key', 
+                              'Handler_read_next', 'Handler_read_rnd','Handler_read_rnd_next', 'Sort_merge_passes', 'Binlog_cache_disk_use', 
+                              'Com_select', 'Com_update', 'Com_insert', 'Com_delete', 'Com_commit', 'Com_rollback']
+            var_metrics = client.get_variables(variables)
+            guage_metrics = client.get_status_metric(gague_status, data_dict1, "GAGUE")
+            counter_metrics = client.get_status_metric(counter_status, data_dict1, "COUNTER")
+            custom_status = client.get_custom_status(data_dict1, data_dict2)
+            custom_metrics = client.get_custom_metrics(data_dict1, innodb_status_format, slave_status_format)
+            payload.extend(var_metrics)
+            payload.extend(guage_metrics)
+            payload.extend(counter_metrics)
+            payload.extend(custom_status)
+            payload.extend(custom_metrics)
+            
+    except:
+        alive_metrics = generate_metric(endpoint, "mysql_alive", ts, 60, 0, "GAGUE", tags)
+        payload.append(alive_metrics)
+
+    print(payload)
+    #r = requests.post("http://127.0.0.1:1988/v1/push", data=json.dumps())
+    # print(r.text)
